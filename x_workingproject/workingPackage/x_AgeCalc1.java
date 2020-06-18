51cafeae7328856dc74a654aa247f335ba85988c
chaarvikk <64976954+chaarvikk@users.noreply.github.com>
2020-06-18 07:34:12 +0530

    commited on 18th 06 2020

---
+++ x_workingproject/workingPackage/x_AgeCalc1.java
@@ -0,0 +1,34 @@
+//package workingPackage;
+
+import java.time.Period;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+
+public class x_AgeCalc1 
+{
+	
+	static String age = "24/09/2007";
+	static String getage1;
+	
+    public static void xage(String inputage) 
+    {
+    	age = inputage;
+
+    	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");    
+    	LocalDate today = LocalDate.now();
+    	
+     	LocalDate birthday = LocalDate.parse(age, formatter);
+
+    	Period p = Period.between(birthday, today);
+    	
+    	getage1 = "You are " + p.getYears() + " years, " + p.getMonths() +
+    	                   " months and " + p.getDays() +
+    	                   " days old.";
+    }
+    
+    public static String getAge() 
+    {
+    	return getage1;	
+    }
+}
---
+++ x_workingproject/workingPackage/x_age1.java
@@ -0,0 +1,14 @@
+
+public class x_age1 {
+	
+	public static void main (String[] args) 
+    {
+//		String age;
+//    	age = x_AgeCalc.xage("24/09.2007");
+		
+		x_AgeCalc1.xage("23/08/2009");
+		x_AgeCalc1.getAge();
+    	
+    	System.out.println("Thank you for using age calculator. " +x_AgeCalc.getAge());
+    }
+    }
